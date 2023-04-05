466e5ede7166a5260fca20d7c9091152c9cf4941
ayman nourredine <ayman251294@gmail.com>
2023-04-05 14:43:10 -0700

    last fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,11 +11,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-	if (*s == 0){
+	if (*s == 0)
 		return (1);
-	}
-	int len = _strlen_recursion(s);
-	return (check_pal(s, 0, len));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
