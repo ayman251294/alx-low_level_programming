02a473309882ef763346f7bd467a744dc4f4990a
ayman nourredine <ayman251294@gmail.com>
2023-04-05 11:16:15 -0700

    new commit about 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,8 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
-		return (actual_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion(n, 0));
 /**
  * actual_sqrt_recursion - recursively finds the square root of a number
  * @n: number to find the square root of
