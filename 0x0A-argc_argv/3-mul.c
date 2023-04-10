83bcb565d81be3fea56491001dbbd38a0e832150
ayman nourredine <ayman251294@gmail.com>
2023-04-10 07:09:45 -0700

    new update 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,23 +3,28 @@
 #include "main.h"
 
 /**
- * main - multiply two numbers
- * @argc: number of argument
- * @argv: array of argument
+ * main - Multiply two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
- * Return: Always 0 (Success), 1 (Error)
+ * Return: 0 if successful, 1 if error occurs
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = (num1 * num2);
-	printf("%d\n", result);
-	return (0);
+    int num1, num2, result;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    result = num1 * num2;
+
+    printf("%d\n", result);
+
+    return (0);
 }
