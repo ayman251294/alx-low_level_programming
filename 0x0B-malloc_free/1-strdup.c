c9fc9d6851c60043550a2d00df0e62ff02ad651a
ayman nourredine <ayman251294@gmail.com>
2023-04-11 09:14:20 -0700

    fixing betty style file 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,23 +12,23 @@ char *_strdup(char *str)
 {
 	char *dup;
 	unsigned int i, len;
-	
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	len = 0;
 	while (*(str + len))
 		len++;
-	
+
 	dup = malloc(sizeof(char) * (len + 1));
-	
+
 	if (dup == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < len; i++)
 		*(dup + i) = *(str + i);
-	
+
 	*(dup + len) = '\0';
-	
+
 	return (dup);
 }
