fdf59c995e7edf23f75df88b94b2fcd85b80e67e
ayman nourredine <ayman251294@gmail.com>
2023-04-11 09:12:05 -0700

    fix the update of file 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,25 +10,25 @@
  */
 char *_strdup(char *str)
 {
-    char *dup;
-    unsigned int i, len;
-
-    if (str == NULL)
-        return (NULL);
-
-    len = 0;
-    while (*(str + len))
-        len++;
-
-    dup = malloc(sizeof(char) * (len + 1));
-
-    if (dup == NULL)
-        return (NULL);
-
-    for (i = 0; i < len; i++)
-        *(dup + i) = *(str + i);
-
-    *(dup + len) = '\0';
-
-    return (dup);
+	char *dup;
+	unsigned int i, len;
+	
+	if (str == NULL)
+		return (NULL);
+	
+	len = 0;
+	while (*(str + len))
+		len++;
+	
+	dup = malloc(sizeof(char) * (len + 1));
+	
+	if (dup == NULL)
+		return (NULL);
+	
+	for (i = 0; i < len; i++)
+		*(dup + i) = *(str + i);
+	
+	*(dup + len) = '\0';
+	
+	return (dup);
 }
