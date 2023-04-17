b7fd96d6ea86cc42474073f6f3885ebdaa3f903e
ayman nourredine <ayman251294@gmail.com>
2023-04-17 07:10:41 -0700

    creat file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic info for a dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
