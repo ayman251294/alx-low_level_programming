9f2f8dc3e24ac389c2bd4a263bb862ac34e610c8
Ayman Nourredine <ayman251294@gmail.com>
2023-05-02 07:13:32 -0700

    new update file 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-	
-	while (h!= NULL)
+
+	while (h! = NULL)
 	{
 		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
-	
+
 	return (num);
+}
