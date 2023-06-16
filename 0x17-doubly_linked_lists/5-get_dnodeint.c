e86c827639f2a21fd03c874ec8f018b1856122c1
Ayman Nourredine <ayman251294@gmail.com>
2023-06-15 19:16:09 -0700

    new update 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,8 @@
 include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * get_dnodeint_at_index - returns the nth node
+ * of a dlistint_t linked list
  * @head: head of the list
  * @index: index of the nth node
  *
